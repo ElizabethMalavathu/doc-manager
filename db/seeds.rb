b31ba1e4a70888fab2d2836c223e048baad5d232
Ben Wilson <ben.wilson@livingsocial.com>
2013-02-24 23:00:36 -0500

    exception handling in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,18 +3,25 @@ documents = file_contents.split("\n\n")
 bibliography = documents.shift
 
 documents.each do |document|
-  lines        = document.split("\n")
-  title        = lines.shift.strip
-  location     = lines.shift.strip
-  date         = Date.parse(lines.shift) rescue nil
-  background   = if lines.first.include?("background: ")
-    lines.shift.gsub("background: ", "").strip
-  end
+  begin
+    lines        = document.split("\n")
+    title        = lines.shift.strip
+    location     = lines.shift.strip
+    date         = Date.parse(lines.shift) rescue nil
+    background   = if lines.first.include?("background: ")
+      lines.shift.gsub("background: ", "").strip
+    end
 
-  PrimaryDocument.create! :title => title,
-                          :location => location,
-                          :publication_date => date,
-                          :content => lines.map(&:strip).join('\n'),
-                          :background => background
+    PrimaryDocument.create! :title => title,
+                            :location => location,
+                            :publication_date => date,
+                            :content => lines.map(&:strip).join('\n'),
+                            :background => background
+  rescue Exception => e
+    puts "==============================================="
+    puts "There was an error with the following document"
+    puts document
+    puts e.backtrace
+  end
 end
 
