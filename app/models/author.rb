a36c1de0b52a405bb18cb54def151a1301a84b12
Ben Wilson <ben.wilson@livingsocial.com>
2013-02-24 20:24:28 -0500

    add full name method

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,8 @@ class Author < ActiveRecord::Base
   attr_accessible :biography, :born, :died, :first_name, :last_name
 
   has_many :primary_documents
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
