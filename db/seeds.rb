4f02ee261ee7680eb9268be22fd650a14585a804
Elizabeth Wilson <elizabeth.wilson212@gmail.com>
2013-02-24 22:08:38 -0500

    stuff not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,2 @@
-documents = File.open("#{Rails.root}/db/seeds/the_fugger_news_letters_1.txt").read
-documents.each do |document|
-  document.new "PrimaryDoc"
+file_contents = File.open("#{Rails.root}/db/seeds/the_fugger_news_letters_1.txt").read
+documents = file_contents.split("\n\n")
